176048663ef27a99077de28ac392e61f6e9dbf2a
Daniel Tonon <dan503.npm@gmail.com>
2025-10-01 10:47:47 +1000

    Moved DependencyPreviewBlock code into DependencyPreviews.tsx
    this will help with styling since the styles are going to be using css modules

--- storybook-addon-package/src/blocks/DependencyPreviews.tsx
+++ storybook-addon-package/src/blocks/DependencyPreviews.tsx
@@ -1,9 +1,12 @@
-import { DepsPreviewBlock } from '../components/DepsPreviewBlock'
+import { useState } from 'react'
+import { PrimaryPreview } from '../components/PrimaryPreview'
+import { StoryLink } from '../components/StoryLink'
 import {
 	DependencyGraphProvider,
 	filterOutStoryFiles,
 	useDependencyGraph,
 } from '../hooks/useDependencyGraph'
+import type { StoryInfo } from '../types'
 
 export function DependencyPreviews() {
 	return (
@@ -28,4 +31,107 @@ function TopLevelDependencyPreviews() {
 			<DepsPreviewBlock deps={usedIn} title="Used in" />
 		</div>
 	)
+}
+
+interface DepsPreviewBlockProps {
+	deps: Array<StoryInfo>
+	title: string
+}
+
+export function DepsPreviewBlock({ deps, title }: DepsPreviewBlockProps) {
+	return (
+		<details>
+			<summary>
+				<h2>
+					{title} {deps.length} component
+					{plural(deps)}
+				</h2>
+			</summary>
+
+			<div>
+				{deps.length ? (
+					<ul>
+						{deps.map((info) => (
+							<DepsPreviewItem
+								storyInfo={info}
+								key={info.componentPath}
+							/>
+						))}
+					</ul>
+				) : (
+					<p>—</p>
+				)}
+			</div>
+		</details>
+	)
+}
+
+interface PropsForDepsPreviewItem {
+	storyInfo: StoryInfo
+}
+
+function DepsPreviewItem({ storyInfo }: PropsForDepsPreviewItem) {
+	const { graph } = useDependencyGraph()
+	const { builtWith, usedIn } = graph![storyInfo.componentPath]
+	const [isOpen, setIsOpen] = useState(false)
+	const filteredBuiltWith = filterOutStoryFiles(builtWith)
+	const fitleredUsedIn = filterOutStoryFiles(usedIn)
+
+	return (
+		<li key={storyInfo.componentPath}>
+			{storyInfo.storyId ? (
+				<details
+					onToggle={(e) => {
+						e.stopPropagation()
+						setIsOpen((prev) => !prev)
+					}}
+				>
+					<summary>
+						<StoryLink info={storyInfo} />
+					</summary>
+					{isOpen && storyInfo.storyId && (
+						<div>
+							<PrimaryPreview storyInfo={storyInfo} />
+							{filteredBuiltWith.length > 0 && (
+								<>
+									<p>Built with:</p>
+									<ul>
+										{filterOutStoryFiles(builtWith).map(
+											(info) => (
+												<DepsPreviewItem
+													storyInfo={info}
+													key={info.componentPath}
+												/>
+											),
+										)}
+									</ul>
+								</>
+							)}
+							{fitleredUsedIn.length > 0 && (
+								<>
+									<p>Used in:</p>
+									<ul>
+										{filterOutStoryFiles(usedIn).map(
+											(info) => (
+												<DepsPreviewItem
+													storyInfo={info}
+													key={info.componentPath}
+												/>
+											),
+										)}
+									</ul>
+								</>
+							)}
+						</div>
+					)}
+				</details>
+			) : (
+				<StoryLink info={storyInfo} />
+			)}
+		</li>
+	)
+}
+
+function plural(arr: Array<any>) {
+	return arr.length === 1 ? '' : 's'
 }
