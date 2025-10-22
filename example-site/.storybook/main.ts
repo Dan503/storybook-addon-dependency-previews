dcb064aadd93efe31691d635763f4861d458d14f
Daniel Tonon <dan503.npm@gmail.com>
2025-10-22 16:26:13 +1100

    removed unecessary extra code from example-site main.ts storybook file

--- example-site/.storybook/main.ts
+++ example-site/.storybook/main.ts
@@ -1,26 +1,4 @@
 import { StorybookConfig } from '@storybook/react-vite'
-import { readFileSync } from 'node:fs'
-import { resolve } from 'node:path'
-
-// Create a Vite plugin that injects lineage.json as a virtual module
-function dependencyPreviewsVirtualPlugin() {
-	const virtId = 'virtual:dependency-previews-json'
-	const resolved = '\0' + virtId
-	return {
-		name: 'dependency-previews-virtual-json',
-		resolveId(id: string) {
-			return id === virtId ? resolved : null
-		},
-		load(id: string) {
-			if (id === resolved) {
-				const jsonPath = resolve(__dirname, './dependency-previews.json')
-				const json = readFileSync(jsonPath, 'utf8')
-				return `export default ${json};`
-			}
-			return null
-		},
-	}
-}
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.stories.@(ts|tsx|mdx|svelte|vue)'],
@@ -34,16 +12,6 @@ const config: StorybookConfig = {
 		options: {},
 	},
 	staticDirs: ['.'],
-	async viteFinal(cfg) {
-		cfg.optimizeDeps ??= {}
-		cfg.optimizeDeps.exclude = [
-			...(cfg.optimizeDeps.exclude ?? []),
-			'storybook-addon-dependency-previews',
-		]
-		cfg.plugins = cfg.plugins || []
-		cfg.plugins.push(dependencyPreviewsVirtualPlugin())
-		return cfg
-	},
 }
 
 export default config
