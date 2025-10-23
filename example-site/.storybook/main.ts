6de1caeabd86b431c6cbea51a5a15047bcac9065
Daniel Tonon <dan503.npm@gmail.com>
2025-10-23 15:33:05 +1100

    staticDirs is not needed anymore and updated tsconfig

--- example-site/.storybook/main.ts
+++ example-site/.storybook/main.ts
@@ -4,14 +4,13 @@ const config: StorybookConfig = {
 	stories: ['../src/**/*.stories.@(ts|tsx|mdx|svelte|vue)'],
 	addons: [
 		'@storybook/addon-docs',
-		// register your addon (panel available in UI)
+		// register your addon
 		'storybook-addon-dependency-previews/addon',
 	],
 	framework: {
 		name: '@storybook/react-vite', // or @storybook/*-vite for your framework
 		options: {},
 	},
-	staticDirs: ['.'],
 }
 
 export default config
