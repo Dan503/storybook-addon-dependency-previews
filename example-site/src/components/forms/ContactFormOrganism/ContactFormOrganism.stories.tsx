e030ed8315eaeb76f9cf78b671094a1f7b82474f
Daniel Tonon <dan503.npm@gmail.com>
2025-10-31 23:56:18 +1100

    ContactFormOrganism component

---
+++ example-site/src/components/forms/ContactFormOrganism/ContactFormOrganism.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { useState } from 'react'
+import { FormDataMolecule } from '../FormDataPreview/FormDataMolecule'
+import {
+	ContactFormOrganism,
+	defaultContactFormValues,
+	type PropsForContactFormOrganism,
+} from './ContactFormOrganism'
+
+const meta: Meta<typeof ContactFormOrganism> = {
+	title: 'Forms / Contact Form Organism',
+	component: ContactFormOrganism,
+	tags: ['autodocs', 'organism'],
+	parameters: {
+		__filePath: import.meta.url,
+	},
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+	args: {} satisfies PropsForContactFormOrganism,
+	render: () => {
+		const [formValues, setFormValues] = useState(defaultContactFormValues)
+
+		return (
+			<FormDataMolecule formValues={formValues}>
+				<ContactFormOrganism onValuesChange={setFormValues} />
+			</FormDataMolecule>
+		)
+	},
+}
---
+++ example-site/src/components/forms/ContactFormOrganism/ContactFormOrganism.tsx
@@ -0,0 +1,72 @@
+import { useForm, useStore } from '@tanstack/react-form'
+import { TextFieldMolecule } from '../TextFieldMolecule/TextFieldMolecule'
+import { TextAreaMolecule } from '../TextAreaMolecule/TextAreaMolecule'
+import { useEffect } from 'react'
+
+export const defaultContactFormValues = {
+	name: '',
+	email: '',
+	message: '',
+}
+
+export type ContactFormValues = typeof defaultContactFormValues
+
+export interface PropsForContactFormOrganism {
+	onSubmit?: () => void
+	onValuesChange?: (values: ContactFormValues) => void
+}
+
+export function ContactFormOrganism({
+	onSubmit,
+	onValuesChange,
+}: PropsForContactFormOrganism) {
+	const form = useForm({
+		defaultValues: defaultContactFormValues,
+		onSubmit: onSubmit,
+	})
+	const values = useStore(form.store, (state) => state.values)
+
+	useEffect(() => {
+		onValuesChange?.(values)
+	}, [values])
+
+	return (
+		<form
+			className="ContactFormOrganism grid gap-4"
+			onSubmit={(event) => {
+				event.preventDefault()
+				form.handleSubmit(event)
+			}}
+		>
+			<form.Field name="name">
+				{(field) => (
+					<TextFieldMolecule
+						label="Name"
+						placeholder="Your name"
+						field={field}
+					/>
+				)}
+			</form.Field>
+
+			<form.Field name="email">
+				{(field) => (
+					<TextFieldMolecule
+						label="Email"
+						placeholder="example@email.com"
+						field={field}
+					/>
+				)}
+			</form.Field>
+
+			<form.Field name="message">
+				{(field) => (
+					<TextAreaMolecule
+						label="Message"
+						placeholder="example@email.com"
+						field={field}
+					/>
+				)}
+			</form.Field>
+		</form>
+	)
+}
