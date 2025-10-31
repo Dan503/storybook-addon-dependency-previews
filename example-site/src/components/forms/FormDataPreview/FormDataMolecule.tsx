48a6b64af2a7cd7331d66a690e8e1482aa2e4f6e
Daniel Tonon <dan503.npm@gmail.com>
2025-10-31 23:55:36 +1100

    FormDataMolecule component changes

--- example-site/src/components/forms/FormDataPreview/FormDataMolecule.tsx
+++ example-site/src/components/forms/FormDataPreview/FormDataMolecule.tsx
@@ -1,16 +1,15 @@
-import type { AnyFormApi } from '@tanstack/react-form'
 import type { ReactNode } from 'react'
 import { FormDataPreviewAtom } from './FormDataPreviewAtom'
 
-interface PropsForFormDataWrapper<FormApi extends AnyFormApi> {
-	formValues?: FormApi['state']['values']
+interface PropsForFormDataWrapper<FormValues extends Record<string, any>> {
+	formValues?: FormValues
 	children: ReactNode
 }
 
-export function FormDataMolecule<FormApi extends AnyFormApi>({
+export function FormDataMolecule<FormValues extends Record<string, any>>({
 	formValues,
 	children,
-}: PropsForFormDataWrapper<FormApi>) {
+}: PropsForFormDataWrapper<FormValues>) {
 	return (
 		<div className="grid gap-2">
 			{children}
