fb76dda6839b3936bfabc6f3e18414bb0133a871
Daniel Tonon <dan503.npm@gmail.com>
2025-11-05 16:05:32 +1100

    CardMolecule styles update

--- example-site/src/components/listings/card/CardMolecule.tsx
+++ example-site/src/components/listings/card/CardMolecule.tsx
@@ -17,12 +17,14 @@ export function CardMolecule({
 	return (
 		<Link
 			to={href}
-			className="flex flex-col gap-2 border rounded-2xl p-4 focus:bg-gray-200 hover:bg-gray-200 hover:shadow-lg hover:transform-[scale(1.02)] transition-all"
+			className="flex flex-col gap-2 border rounded-2xl overflow-hidden focus:bg-gray-200 hover:bg-gray-200 hover:shadow-lg hover:transform-[scale(1.02)] transition-all"
 		>
-			<img src={imgSrc} alt="" />
-			<Section component={<H className="text-3xl font-bold">{title}</H>}>
-				<p className="line-clamp-4">{description}</p>
-			</Section>
+			<img src={imgSrc} alt="" className="aspect-video object-cover" />
+			<div className="p-4">
+				<Section component={<H className="text-3xl font-bold">{title}</H>}>
+					<p className="line-clamp-4">{description}</p>
+				</Section>
+			</div>
 		</Link>
 	)
 }
