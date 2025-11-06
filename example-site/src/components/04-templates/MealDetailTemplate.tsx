ff68d0bff66641b6c8890082867fc973f692d1dd
Daniel Tonon <dan503.npm@gmail.com>
2025-11-06 15:42:30 +1100

    Improved the layout of the meal detail template

--- example-site/src/components/04-templates/MealDetailTemplate.tsx
+++ example-site/src/components/04-templates/MealDetailTemplate.tsx
@@ -2,6 +2,7 @@ import { H, Section } from 'react-headings'
 import type { Meal } from '../../utils/mealDbApiUtils'
 import { CompactListingOrganism } from '../listings/compact/CompactListingOrganism'
 import { SiteFrameOrganism } from '../03-organisms/SiteFrameOrganism'
+import { ScreenPaddingAtom } from '../01-atoms/ScreenPaddingAtom'
 
 export interface PropsForMealDetailTemplate {
 	meal: Meal | undefined
@@ -18,16 +19,20 @@ export function MealDetailTemplate({
 
 	return (
 		<SiteFrameOrganism>
-			<div className="grid gap-4">
+			<ScreenPaddingAtom padVertical>
 				<Section component={<H className="text-3xl font-bold">{meal.name}</H>}>
-					<div className="grid gap-4 md:grid-cols-[1fr_2fr]">
-						<img src={meal.image} alt={meal.name} className="mt-2" />
-						<div>
-							<Section component={<H className="text-2xl font-bold">Recipe</H>}>
-								<p className="whitespace-pre-wrap">{meal.instructions}</p>
-							</Section>
+					<div className="grid gap-4 lg:grid-cols-[2fr_30rem]">
+						<div className="grid gap-4 sm:grid-cols-[1fr_2fr]">
+							<img src={meal.image} alt={meal.name} className="mt-2" />
+							<div>
+								<Section
+									component={<H className="text-2xl font-bold">Recipe</H>}
+								>
+									<p className="whitespace-pre-wrap">{meal.instructions}</p>
+								</Section>
+							</div>
 						</div>
-						<div className="grid gap-4 md:col-span-2">
+						<div className="grid gap-4 grid-rows-[auto_1fr]">
 							<Section
 								component={<H className="text-2xl font-bold">Ingredients</H>}
 							>
@@ -42,7 +47,7 @@ export function MealDetailTemplate({
 						</div>
 					</div>
 				</Section>
-			</div>
+			</ScreenPaddingAtom>
 		</SiteFrameOrganism>
 	)
 }
