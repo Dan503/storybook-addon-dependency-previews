cf2a9d2ef2500f21c4a849b4811f750c1c89553d
Daniel Tonon <c_dtonon@qtccomau.onmicrosoft.com>
2026-01-13 17:09:18 +1100

    footer component done

--- example-site/src/components/03-organisms/FooterOrganism.tsx
+++ example-site/src/components/03-organisms/FooterOrganism.tsx
@@ -1,14 +1,24 @@
 import type { ReactNode } from 'react'
 
 export interface PropsForFooterOrganism {
-  children?: ReactNode
+	children?: ReactNode
 }
 
 export function FooterOrganism({ children }: PropsForFooterOrganism) {
-  return (
-    <div className="FooterOrganism">
-      <p>FooterOrganism</p>
-      {children}
-    </div>
-  )
+	return (
+		<div className="FooterOrganism border-t-2 border-teal-900 bg-teal-200 p-4 text-center text-black">
+			<p>
+				Meal data provided by{' '}
+				<a
+					href="https://www.themealdb.com/"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="underline hover:no-underline focus:no-underline"
+				>
+					TheMealDB.com
+				</a>
+			</p>
+			{children}
+		</div>
+	)
 }
