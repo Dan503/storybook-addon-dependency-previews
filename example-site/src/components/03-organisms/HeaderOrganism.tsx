15cf913f00444097d26b54a5e1f7981b6cf7f2db
Daniel Tonon <dan503.npm@gmail.com>
2026-01-15 14:22:34 +1100

    Logo links to the home page

--- example-site/src/components/03-organisms/HeaderOrganism.tsx
+++ example-site/src/components/03-organisms/HeaderOrganism.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@tanstack/react-router'
 import { ScreenPaddingAtom } from '../01-atoms/ScreenPaddingAtom'
 import { MainNavMolecule } from '../02-molecules/MainNavMolecule'
 
@@ -6,10 +7,10 @@ export function HeaderOrganism() {
 		<header className="p-2 bg-teal-100 border-b-2 border-teal-900 text-black">
 			<ScreenPaddingAtom>
 				<div className="flex w-full items-center justify-between gap-2 max-md:flex-col max-md:justify-center">
-					<div className="flex items-center gap-2">
+					<Link to="/" className="flex items-center gap-2">
 						<img src="/simplified-logo.png" alt="Logo" height={50} width={50} />
 						<p className="font-extrabold text-3xl">The Meal Place</p>
-					</div>
+					</Link>
 					<MainNavMolecule />
 				</div>
 			</ScreenPaddingAtom>
