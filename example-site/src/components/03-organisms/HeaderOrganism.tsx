fd4a1add0e69bde4721568baf985f135afa6fee5
Daniel Tonon <dan503.npm@gmail.com>
2026-01-15 13:25:25 +1100

    Added logo as favicon

--- example-site/src/components/03-organisms/HeaderOrganism.tsx
+++ example-site/src/components/03-organisms/HeaderOrganism.tsx
@@ -7,7 +7,7 @@ export function HeaderOrganism() {
 			<ScreenPaddingAtom>
 				<div className="flex w-full items-center justify-between gap-2 max-md:flex-col max-md:justify-center">
 					<div className="flex items-center gap-2">
-						<img src="/logo.png" alt="Logo" height={50} width={50} />
+						<img src="/simplified-logo.png" alt="Logo" height={50} width={50} />
 						<p className="font-extrabold text-3xl">The Meal Place</p>
 					</div>
 					<MainNavMolecule />
