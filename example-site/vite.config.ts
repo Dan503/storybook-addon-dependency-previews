2c8414aae934b36dd3750954e92ee4978c0cb040
Daniel Tonon <dan503.npm@gmail.com>
2026-01-30 13:25:08 +1100

    Set up Netlify publishing

--- example-site/vite.config.ts
+++ example-site/vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
-  plugins: [
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
+	plugins: [
+		// this is the plugin that enables path aliases
+		viteTsConfigPaths({
+			projects: ['./tsconfig.json'],
+		}),
+		tailwindcss(),
+		tanstackStart(),
+		netlify(),
+		viteReact(),
+	],
 })
 
 export default config
