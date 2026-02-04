0a7a5808e51a5a0393841f7b87218eedfb610119
Daniel Tonon <dan503.npm@gmail.com>
2026-02-04 11:19:08 +1100

    Phone Icon added

---
+++ example-site/svelte/src/components/01-atoms/icons/PhoneIcon.stories.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" module>
+	import type { StoryParameters } from 'storybook-addon-dependency-previews';
+	import PhoneIcon from './PhoneIcon.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: '01 Atoms / Icons / Phone Icon',
+		component: PhoneIcon,
+		tags: ['autodocs', 'icon'],
+		parameters: {
+			layout: 'centered',
+			__filePath: import.meta.url
+		} satisfies StoryParameters
+	});
+</script>
+
+<Story name="Primary" />
