426b7dc7fa09c1470a88ec934d825abbe1b82058
Daniel Tonon <dan503.npm@gmail.com>
2026-02-04 15:25:09 +1100

    SiteFrameOrganism

---
+++ example-site/svelte/src/components/03-organisms/SiteFrameOrganism.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import HeaderOrganism from './HeaderOrganism.svelte';
+	import FooterOrganism from './FooterOrganism.svelte';
+
+	export interface PropsForSiteFrameOrganism {
+		children?: Snippet;
+	}
+
+	const { children }: PropsForSiteFrameOrganism = $props();
+</script>
+
+<div class="SiteFrameOrganism grid min-h-full grid-rows-[auto_1fr_auto]">
+	<HeaderOrganism />
+	<div class="grid flex-1">
+		{@render children?.()}
+	</div>
+	<FooterOrganism />
+</div>
