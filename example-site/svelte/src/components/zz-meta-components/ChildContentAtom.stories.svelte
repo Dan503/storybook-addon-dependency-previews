5b31c639f9df732ebffe033ea5b33846d4cf90e7
Daniel Tonon <dan503.npm@gmail.com>
2026-02-04 11:41:02 +1100

    child content meta component

---
+++ example-site/svelte/src/components/zz-meta-components/ChildContentAtom.stories.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" module>
+	import type { StoryParameters } from 'storybook-addon-dependency-previews';
+	import ChildContentAtom from './ChildContentAtom.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'Zz Meta Components / Child Content Atom',
+		component: ChildContentAtom,
+		tags: ['autodocs', 'atom'],
+		parameters: {
+			layout: 'padded',
+			__filePath: import.meta.url
+		} satisfies StoryParameters
+	});
+</script>
+
+<Story name="Primary" />
---
+++ example-site/svelte/src/components/zz-meta-components/ChildContentAtom.svelte
@@ -0,0 +1,5 @@
+<div
+	class="ChildContentAtom grid min-h-60 place-items-center rounded-md border-2 border-dashed bg-gray-200 p-4 text-black"
+>
+	<p>Placeholder child content</p>
+</div>
