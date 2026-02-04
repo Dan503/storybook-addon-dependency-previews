5227620cf41658a6057f794a453cfb8fe9c42f2e
Daniel Tonon <dan503.npm@gmail.com>
2026-02-04 11:48:35 +1100

    Content restraint and screen padding components

---
+++ example-site/svelte/src/components/01-atoms/ContentRestraintAtom.stories.svelte
@@ -0,0 +1,20 @@
+<script lang="ts" module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import type { StoryParameters } from 'storybook-addon-dependency-previews';
+	import ChildContentAtom from '../zz-meta-components/ChildContentAtom.svelte';
+	import ContentRestraintAtom from './ContentRestraintAtom.svelte';
+
+	const { Story } = defineMeta({
+		title: '01 Atoms / Content Restraint Atom',
+		component: ContentRestraintAtom,
+		tags: ['autodocs', 'atom'],
+		parameters: {
+			layout: 'fullscreen',
+			__filePath: import.meta.url
+		} satisfies StoryParameters
+	});
+</script>
+
+<Story name="Primary">
+	<ChildContentAtom />
+</Story>
