d00ad9c97366b0ce05aa942781996308615ac973
Daniel Tonon <dan503.npm@gmail.com>
2026-02-04 14:33:50 +1100

    Main Nav Molecule Svelte component

---
+++ example-site/svelte/src/components/02-molecules/MainNavMolecule.stories.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import type { StoryParameters } from 'storybook-addon-dependency-previews';
+	import MainNavMolecule from './MainNavMolecule.svelte';
+
+	const { Story } = defineMeta({
+		title: '02 Molecules / Main Nav Molecule',
+		component: MainNavMolecule,
+		tags: ['autodocs', 'molecule'],
+		parameters: {
+			layout: 'padded',
+			__filePath: import.meta.url
+		} satisfies StoryParameters
+	});
+</script>
+
+<Story name="Primary" />
---
+++ example-site/svelte/src/components/02-molecules/MainNavMolecule.svelte
@@ -0,0 +1,13 @@
+<nav class="flex flex-row">
+	<div class="px-2 font-bold">
+		<a href="/">Home</a>
+	</div>
+
+	<div class="px-2 font-bold">
+		<a href="/categories">Food categories</a>
+	</div>
+
+	<div class="px-2 font-bold">
+		<a href="/contact">Contact us</a>
+	</div>
+</nav>
