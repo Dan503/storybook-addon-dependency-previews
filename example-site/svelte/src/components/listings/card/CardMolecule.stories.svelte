996dd42b697e72995b4abe2e6cfebf5174a37734
Daniel Tonon <dan503.npm@gmail.com>
2026-02-05 10:58:25 +1100

    Svelte Card Molecule component created

---
+++ example-site/svelte/src/components/listings/card/CardMolecule.stories.svelte
@@ -0,0 +1,43 @@
+<script lang="ts" module>
+	import type { StoryParameters } from 'storybook-addon-dependency-previews';
+	import CardMolecule, { type PropsForCardMolecule } from './CardMolecule.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { exampleMeal } from 'example-site-shared/data';
+
+	const { Story } = defineMeta({
+		title: 'Listings / Card / Card Molecule',
+		component: CardMolecule,
+		tags: ['autodocs', 'molecule'],
+		parameters: {
+			layout: 'padded',
+			__filePath: import.meta.url
+		} satisfies StoryParameters
+	});
+	type Args = Omit<PropsForCardMolecule, 'children'>;
+</script>
+
+<Story
+	name="Primary"
+	args={{
+		title: 'Title of the card',
+		href: '/',
+		description: `Card description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+		imgSrc: exampleMeal.image
+	} satisfies Args}
+>
+	{#snippet template(args)}
+		<div style:width="400px" class="mx-auto border border-dashed border-gray-500 p-4">
+			<CardMolecule {...args} />
+		</div>
+	{/snippet}
+</Story>
+
+<Story
+	name="Full width"
+	args={{
+		title: 'Title of the card',
+		href: '/',
+		description: `Card description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+		imgSrc: exampleMeal.image
+	} satisfies Args}
+/>
---
+++ example-site/svelte/src/components/listings/card/CardMolecule.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	export interface PropsForCardMolecule {
+		title: string;
+		imgSrc: string;
+		description: string;
+		href: string;
+	}
+
+	const { title, imgSrc, description, href }: PropsForCardMolecule = $props();
+</script>
+
+<div class="CardMolecule @container grid">
+	<a
+		{href}
+		class="flex h-full gap-2 overflow-hidden rounded-2xl border bg-white transition-all hover:transform-[scale(1.02)] hover:bg-teal-200 hover:shadow-lg focus:bg-teal-200"
+	>
+		<img src={imgSrc} alt="" class="aspect-video object-cover" />
+		<div class="w-full p-4">
+			<h3 class="text-xl font-bold">{title}</h3>
+			<p class="line-clamp-4">{description}</p>
+		</div>
+	</a>
+</div>
+
+<style>
+	.CardMolecule {
+		@container (400px <= width) {
+			img {
+				width: 200px;
+				aspect-ratio: 1 / 1;
+			}
+		}
+		@container (width < 400px) {
+			a {
+				flex-direction: column;
+			}
+			img {
+				width: 100%;
+				aspect-ratio: 16 / 9;
+			}
+		}
+	}
+</style>
