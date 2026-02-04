56476021df088f2e3eac97586e4f6d1cdfc3f0d6
Daniel Tonon <dan503.npm@gmail.com>
2026-02-04 15:14:07 +1100

    Header organism

---
+++ example-site/svelte/src/components/03-organisms/HeaderOrganism.stories.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import type { StoryParameters } from 'storybook-addon-dependency-previews';
+	import HeaderOrganism from './HeaderOrganism.svelte';
+
+	const { Story } = defineMeta({
+		title: '03 Organisms / Header Organism',
+		component: HeaderOrganism,
+		tags: ['autodocs', 'organism'],
+		parameters: {
+			layout: 'padded',
+			__filePath: import.meta.url
+		} satisfies StoryParameters
+	});
+</script>
+
+<Story name="Primary" />
