ee915b695015e49b8a1c9f66e8a547f6b9dc54da
Daniel Tonon <dan503.npm@gmail.com>
2026-02-04 11:33:09 +1100

    BgImageContainerAtom

---
+++ example-site/svelte/src/components/01-atoms/BgImageContainerAtom.stories.svelte
@@ -0,0 +1,30 @@
+<script lang="ts" module>
+	import type { StoryParameters } from 'storybook-addon-dependency-previews';
+	import BgImageContainerAtom, {
+		type PropsForBgImageContainerAtom
+	} from './BgImageContainerAtom.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: '01 Atoms / Bg Image Container Atom',
+		component: BgImageContainerAtom,
+		tags: ['autodocs', 'atom'],
+		parameters: {
+			layout: 'padded',
+			__filePath: import.meta.url
+		} satisfies StoryParameters
+	});
+	type Args = Omit<PropsForBgImageContainerAtom, 'children'>;
+</script>
+
+<Story
+	name="Primary"
+	args={{
+		imgSrc: 'https://www.themealdb.com/images/media/meals/wyxwsp1486979827.jpg',
+		altText: 'Placeholder Image'
+	} satisfies Args}
+>
+	<p class="border-2 border-dashed border-red-500 p-4 text-2xl font-bold text-black">
+		Content inside BgImageContainer
+	</p>
+</Story>
---
+++ example-site/svelte/src/components/01-atoms/BgImageContainerAtom.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	export interface PropsForBgImageContainerAtom {
+		imgSrc?: string;
+		altText?: string;
+		className?: string;
+		innerClassName?: string;
+		tintColor?: string;
+		tintPercent?: number;
+		children?: Snippet;
+	}
+
+	const {
+		children,
+		imgSrc,
+		altText = '',
+		className = '',
+		tintColor = 'white',
+		tintPercent = 70,
+		innerClassName = ''
+	}: PropsForBgImageContainerAtom = $props();
+</script>
+
+<div class={`BgImageContainerAtom relative ${className}`}>
+	<img src={imgSrc} alt={altText} class="absolute top-0 left-0 h-full w-full object-cover" />
+	<div
+		class="absolute top-0 left-0 h-full w-full"
+		style:background-color={tintColor}
+		style:opacity={`${tintPercent}%`}
+	></div>
+	<div class={`relative z-10 grid ${innerClassName}`}>
+		{@render children?.()}
+	</div>
+</div>
