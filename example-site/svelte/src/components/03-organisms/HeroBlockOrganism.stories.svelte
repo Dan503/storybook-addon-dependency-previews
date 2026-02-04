23252c01e636b6df4137076f55a6c848ac0cd67e
Daniel Tonon <dan503.npm@gmail.com>
2026-02-04 15:20:38 +1100

    Hero Block Organism

---
+++ example-site/svelte/src/components/03-organisms/HeroBlockOrganism.stories.svelte
@@ -0,0 +1,26 @@
+<script lang="ts" module>
+	import type { StoryParameters } from 'storybook-addon-dependency-previews';
+	import HeroBlockOrganism, { type PropsForHeroBlockOrganism } from './HeroBlockOrganism.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: '03 Organisms / Hero Block Organism',
+		component: HeroBlockOrganism,
+		tags: ['autodocs', 'organism'],
+		parameters: {
+			layout: 'padded',
+			__filePath: import.meta.url
+		} satisfies StoryParameters
+	});
+	type Args = Omit<PropsForHeroBlockOrganism, 'children'>;
+</script>
+
+<Story
+	name="Primary"
+	args={{
+		title: 'Hero Block Title',
+		imgSrc: 'https://www.themealdb.com/images/media/meals/wyxwsp1486979827.jpg'
+	} satisfies Args}
+>
+	<p>This is where the hero block content goes.</p>
+</Story>
---
+++ example-site/svelte/src/components/03-organisms/HeroBlockOrganism.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import BgImageContainerAtom from '../01-atoms/BgImageContainerAtom.svelte';
+	import ScreenPaddingAtom from '../01-atoms/ScreenPaddingAtom.svelte';
+
+	export interface PropsForHeroBlockOrganism {
+		title: string | Snippet;
+		imgSrc?: string;
+		tintPercent?: number;
+		tintColor?: string;
+		altText?: string;
+		children?: Snippet;
+	}
+
+	const { children, title, imgSrc, tintPercent, tintColor, altText }: PropsForHeroBlockOrganism =
+		$props();
+</script>
+
+<BgImageContainerAtom
+	className="HeroBlockOrganism text-center w-full min-h-100 grid place-items-center border-b-2 border-teal-900"
+	{imgSrc}
+	{altText}
+	{tintColor}
+	{tintPercent}
+>
+	<ScreenPaddingAtom padVertical>
+		<h1 class="text-4xl font-bold">{title}</h1>
+		{@render children?.()}
+	</ScreenPaddingAtom>
+</BgImageContainerAtom>
