287675cf9d1e839f40bd2121391af772428cb81e
Daniel Tonon <dan503.npm@gmail.com>
2026-02-06 10:47:08 +1100

    CompactListingOrganism.svelte added

---
+++ example-site/svelte/src/components/listings/compact/CompactListingOrganism.stories.svelte
@@ -0,0 +1,40 @@
+<script lang="ts" module>
+	import type { StoryParameters } from 'storybook-addon-dependency-previews';
+	import CompactListingOrganism, {
+		type PropsForCompactListingOrganism
+	} from './CompactListingOrganism.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { ingredientItems } from 'example-site-shared/data';
+
+	const { Story } = defineMeta({
+		title: 'Listings / Compact / Compact Listing Organism',
+		component: CompactListingOrganism,
+		tags: ['autodocs', 'organism'],
+		parameters: {
+			layout: 'padded',
+			__filePath: import.meta.url
+		} satisfies StoryParameters,
+		argTypes: {
+			// Use mapping to prevent large data from being serialized into URL
+			items: {
+				mapping: {
+					ingredients: ingredientItems
+				},
+				control: {
+					type: 'select'
+				},
+				options: ['ingredients']
+			}
+		}
+	});
+	type Args = Omit<PropsForCompactListingOrganism, 'children'>;
+</script>
+
+<Story
+	name="Primary"
+	args={{
+		items: 'ingredients' as any
+	} satisfies Args}
+>
+	<p>Listings / Compact / Compact Listing Organism</p>
+</Story>
---
+++ example-site/svelte/src/components/listings/compact/CompactListingOrganism.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { PropsForCompactListingMolecule } from './CompactListingMolecule.svelte';
+	import CompactListingMolecule from './CompactListingMolecule.svelte';
+
+	export interface PropsForCompactListingOrganism {
+		items: Array<PropsForCompactListingMolecule>;
+	}
+
+	const { items }: PropsForCompactListingOrganism = $props();
+</script>
+
+<ul class="CompactListingOrganism gap-4">
+	{#each items as item (item.title)}
+		<li>
+			<CompactListingMolecule {...item} />
+		</li>
+	{/each}
+</ul>
+
+<style>
+	.CompactListingOrganism {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+	}
+</style>
