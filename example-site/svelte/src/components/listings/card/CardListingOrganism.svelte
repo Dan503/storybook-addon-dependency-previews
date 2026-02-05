6624224d7a9bad2ff656bd727efc2706c780cc24
Daniel Tonon <dan503.npm@gmail.com>
2026-02-05 11:24:23 +1100

    Card Listing Organism svelte component

---
+++ example-site/svelte/src/components/listings/card/CardListingOrganism.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import CardMolecule, { type PropsForCardMolecule } from './CardMolecule.svelte';
+
+	export interface PropsForCardListingOrganism {
+		cards: Array<PropsForCardMolecule> | undefined;
+	}
+
+	const { cards }: PropsForCardListingOrganism = $props();
+</script>
+
+<div class="@container grid">
+	<div class={`CardListingOrganism grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6`}>
+		{#each cards as card}
+			<CardMolecule {...card} />
+		{/each}
+	</div>
+</div>
+
+<style>
+	.CardListingOrganism {
+		grid-template-columns: repeat(3, minmax(0, 1fr));
+
+		@container (width < 800px) {
+			grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+		}
+
+		@container (width > 1400px) {
+			grid-template-columns: repeat(6, minmax(0, 1fr));
+		}
+	}
+</style>
