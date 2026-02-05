4545d290f0368839d4affa0219067d9b55a33604
Daniel Tonon <dan503.npm@gmail.com>
2026-02-06 10:56:16 +1100

    ErrorMessageAtom.svelte

---
+++ example-site/svelte/src/components/forms/ErrorMessages/ErrorMessageAtom.stories.svelte
@@ -0,0 +1,27 @@
+<script lang="ts" module>
+	import type { StoryParameters } from 'storybook-addon-dependency-previews';
+	import ErrorMessageAtom, { type PropsForErrorMessageAtom } from './ErrorMessageAtom.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'Forms / Error Messages / Error Message Atom',
+		component: ErrorMessageAtom,
+		tags: ['autodocs', 'atom'],
+		parameters: {
+			layout: 'padded',
+			__filePath: import.meta.url
+		} satisfies StoryParameters
+	});
+	type Args = Omit<PropsForErrorMessageAtom, 'children'>;
+</script>
+
+<Story name="Error string" args={{ error: 'This is an error message as a string' } satisfies Args}>
+	<p>Forms / Error Messages / Error Message Atom</p>
+</Story>
+
+<Story
+	name="Error object"
+	args={{ error: new Error('This is an error message as an Error() object') } satisfies Args}
+>
+	<p>Forms / Error Messages / Error Message Atom</p>
+</Story>
