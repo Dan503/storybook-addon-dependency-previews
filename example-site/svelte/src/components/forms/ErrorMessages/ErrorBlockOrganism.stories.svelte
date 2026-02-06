4d5177c54dcecf76b3d10e6c4369709e77d4352c
Daniel Tonon <dan503.npm@gmail.com>
2026-02-06 12:07:04 +1100

    ErrorBlockOrganism.svelte

---
+++ example-site/svelte/src/components/forms/ErrorMessages/ErrorBlockOrganism.stories.svelte
@@ -0,0 +1,24 @@
+<script lang="ts" module>
+	import type { StoryParameters } from 'storybook-addon-dependency-previews';
+	import ErrorBlockOrganism, { type PropsForErrorBlockOrganism } from './ErrorBlockOrganism.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'Forms / Error Messages / Error Block Organism',
+		component: ErrorBlockOrganism,
+		tags: ['autodocs', 'organism'],
+		parameters: {
+			layout: 'padded',
+			__filePath: import.meta.url
+		} satisfies StoryParameters
+	});
+	type Args = Omit<PropsForErrorBlockOrganism, 'children'>;
+	const errorStrings = ['Error One', 'Second error'];
+	const errorObjects = errorStrings.map((err) => new Error(err));
+</script>
+
+<Story name="Error Strings" args={{ errors: errorStrings } satisfies Args} />
+
+<Story name="Error Objects" args={{ errors: errorObjects } satisfies Args} />
+
+<Story name="No Errors" args={{ errors: [] } satisfies Args} />
---
+++ example-site/svelte/src/components/forms/ErrorMessages/ErrorBlockOrganism.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { H, Level } from 'svelte-headings';
+	import ErrorListMolecule from './ErrorListMolecule.svelte';
+
+	export interface PropsForErrorBlockOrganism {
+		errors: Array<string | Error>;
+	}
+
+	const { errors }: PropsForErrorBlockOrganism = $props();
+	console.log('Errors passed to ErrorBlockOrganism:', errors);
+</script>
+
+<div role="alert" class="ErrorBlockOrganism">
+	{#if errors.length > 0}
+		<Level class="rounded-xl bg-red-100 px-4 pt-2">
+			<H class="border-b-2 border-red-800 pb-1 text-2xl font-bold">
+				Please resolve the following errors
+			</H>
+			<div class="pt-3 pb-4">
+				<ErrorListMolecule {errors} />
+			</div>
+		</Level>
+	{/if}
+</div>
