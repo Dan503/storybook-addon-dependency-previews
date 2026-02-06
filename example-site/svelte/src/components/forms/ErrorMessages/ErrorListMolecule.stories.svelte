99f521ebb30591d5a380f10862ff5f3d2c39612b
Daniel Tonon <dan503.npm@gmail.com>
2026-02-06 11:03:11 +1100

    ErrorListMolecule.svelte

---
+++ example-site/svelte/src/components/forms/ErrorMessages/ErrorListMolecule.stories.svelte
@@ -0,0 +1,23 @@
+<script lang="ts" module>
+	import type { StoryParameters } from 'storybook-addon-dependency-previews';
+	import ErrorListMolecule, { type PropsForErrorListMolecule } from './ErrorListMolecule.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'Forms / Error Messages / Error List Molecule',
+		component: ErrorListMolecule,
+		tags: ['autodocs', 'molecule'],
+		parameters: {
+			layout: 'padded',
+			__filePath: import.meta.url
+		} satisfies StoryParameters
+	});
+	type Args = Omit<PropsForErrorListMolecule, 'children'>;
+
+	const errorStrings = ['Error One', 'Second error'];
+	const errorObjects = errorStrings.map((err) => new Error(err));
+</script>
+
+<Story name="Error Strings" args={{ errors: errorStrings } satisfies Args} />
+
+<Story name="Error Objects" args={{ errors: errorObjects } satisfies Args} />
---
+++ example-site/svelte/src/components/forms/ErrorMessages/ErrorListMolecule.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import ErrorMessageAtom from './ErrorMessageAtom.svelte';
+
+	export interface PropsForErrorListMolecule {
+		errors: Array<string | Error>;
+	}
+
+	const { errors }: PropsForErrorListMolecule = $props();
+</script>
+
+<ul class="ErrorListMolecule grid gap-1 pl-6">
+	{#each errors as err}
+		<li class="list-outside list-disc">
+			<ErrorMessageAtom error={err} />
+		</li>
+	{/each}
+</ul>
