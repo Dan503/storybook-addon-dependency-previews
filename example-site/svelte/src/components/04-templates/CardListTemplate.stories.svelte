1ca14abdfa037a04f5211a47c796144ae0e6198f
Daniel Tonon <dan503.npm@gmail.com>
2026-02-13 11:40:33 +1100

    CardListTemplate.svelte

---
+++ example-site/svelte/src/components/04-templates/CardListTemplate.stories.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" module>
+	import type { StoryParameters } from 'storybook-addon-dependency-previews';
+	import CardListTemplate from './CardListTemplate.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: '04 Templates / Card List Template',
+		component: CardListTemplate,
+		tags: ['autodocs', 'template'],
+		parameters: {
+			layout: 'padded',
+			__filePath: import.meta.url
+		} satisfies StoryParameters
+	});
+</script>
+
+<Story name="Primary" />
---
+++ example-site/svelte/src/components/04-templates/CardListTemplate.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { H, Level } from 'svelte-headings';
+	import SiteFrameOrganism from '../03-organisms/SiteFrameOrganism.svelte';
+	import ScreenPaddingAtom from '../01-atoms/ScreenPaddingAtom.svelte';
+	import CardListingOrganism from '../listings/card/CardListingOrganism.svelte';
+	import type { PropsForCardMolecule } from '../listings/card/CardMolecule.svelte';
+
+	export interface PropsForCardListTemplate {
+		title: string;
+		introText?: string;
+		cardList?: Array<PropsForCardMolecule>;
+	}
+
+	const { title, cardList, introText }: PropsForCardListTemplate = $props();
+</script>
+
+<SiteFrameOrganism>
+	<ScreenPaddingAtom padVertical>
+		<Level class="MealListTemplate grid gap-4">
+			<H class="text-4xl font-bold">{title}</H>
+			<p class="mb-2">{introText}</p>
+			<CardListingOrganism cards={cardList} />
+		</Level>
+	</ScreenPaddingAtom>
+</SiteFrameOrganism>
