5fceb817093988ab5c478165cb13b777ba1e05a6
Daniel Tonon <dan503.npm@gmail.com>
2026-02-20 15:38:32 +1100

    Contact page

---
+++ example-site/svelte/src/routes/contact/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import ContactTemplate from '../../components/04-templates/ContactTemplate.svelte';
+</script>
+
+<svelte:head>
+	<title
+		>Contact Us - The Meal Place - [SVELTE] The Storybook Dependency Previews Example Site</title
+	>
+	<meta
+		name="description"
+		content="Get in touch with us at The Meal Place! We'd love to hear from you, whether you have questions, feedback, or just want to say hello."
+	/>
+</svelte:head>
+
+<ContactTemplate />
