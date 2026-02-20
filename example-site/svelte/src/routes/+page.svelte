e7013c8020c9b90b4382e5efc6d62df2f82f0c6a
Daniel Tonon <dan503.npm@gmail.com>
2026-02-20 15:29:09 +1100

    Created the page.svelte homepage

--- example-site/svelte/src/routes/+page.svelte
+++ example-site/svelte/src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import HomeTemplate from '../components/04-templates/HomeTemplate.svelte';
+	import type { PageProps } from './$types';
+
+	const { data }: PageProps = $props();
+</script>
+
+<svelte:head>
+	<title>The Meal Place - [SVELTE] The Storybook Dependency Previews Example Site</title>
+	<meta
+		name="description"
+		content="This is an example site to demonstrate the dependency preview addon in a realistic environment."
+	/>
+</svelte:head>
+
+<HomeTemplate featuredMeals={data.featuredMeals} />
