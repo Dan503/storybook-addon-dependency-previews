b286fa99f2e40b54dd4b674dee0b044c47a29f93
Daniel Tonon <dan503.npm@gmail.com>
2026-02-20 13:59:41 +1100

    Fixed CardListTemplate not displaying card data

--- example-site/svelte/src/components/04-templates/CardListTemplate.stories.svelte
+++ example-site/svelte/src/components/04-templates/CardListTemplate.stories.svelte
@@ -2,6 +2,7 @@
 	import type { StoryParameters } from 'storybook-addon-dependency-previews';
 	import CardListTemplate from './CardListTemplate.svelte';
 	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { categoryCardList, mealCardList } from 'example-site-shared/data';
 
 	const { Story } = defineMeta({
 		title: '04 Templates / Card List Template',
@@ -10,8 +11,23 @@
 		parameters: {
 			layout: 'padded',
 			__filePath: import.meta.url
-		} satisfies StoryParameters
+		} satisfies StoryParameters,
+		argTypes: {
+			// Use mapping to prevent large data from being serialized into URL
+			cardList: {
+				mapping: {
+					categories: categoryCardList,
+					meals: mealCardList
+				},
+				control: {
+					type: 'select'
+				},
+				options: ['categories', 'meals']
+			}
+		}
 	});
 </script>
 
-<Story name="Primary" />
+<Story name="Category list" args={{ cardList: 'categories' as any }} />
+
+<Story name="Meal list" args={{ cardList: 'meals' as any }} />
