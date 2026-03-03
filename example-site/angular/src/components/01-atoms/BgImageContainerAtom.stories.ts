9542de228a8f8a124b27e011ce9e595bf0964b04
Daniel Tonon <dan503.npm@gmail.com>
2026-03-03 17:24:49 +1100

    Updated the BG image Container atom styles

--- example-site/angular/src/components/01-atoms/BgImageContainerAtom.stories.ts
+++ example-site/angular/src/components/01-atoms/BgImageContainerAtom.stories.ts
@@ -19,6 +19,8 @@ type Story = StoryObj<BgImageContainerAtomComponent>
 export const Primary: Story = {
 	render: (args) => ({
 		props: args,
-		template: `<app-bg-image-container-atom imgSrc="https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg" altText="A tasty meal" className="min-h-64 w-full" [tintPercent]="50"><p class="text-white text-2xl font-bold p-4">Content on top</p></app-bg-image-container-atom>`,
+		template: `<app-bg-image-container-atom imgSrc="https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg" altText="A tasty meal" className="min-h-64 w-full" [tintPercent]="50">
+			<p>Content on top</p>
+		</app-bg-image-container-atom>`,
 	}),
 }
