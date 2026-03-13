d370268f25812ba4df22bd1738f357177915dfe9
Daniel Tonon <dan503.npm@gmail.com>
2026-03-13 16:18:02 +1100

    SvgAtom component

---
+++ example-site/angular/src/components/01-atoms/icons/SvgAtom.component.ts
@@ -0,0 +1,28 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'svg-atom',
+  template: `
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      [attr.viewBox]="['0', '0', size(), size()].join(' ')"
+      stroke-width="1.5"
+      stroke="currentColor"
+      [attr.height]="size()"
+      [attr.width]="size()"
+      focusable="false"
+      role="img"
+      [class]="class()"
+      [attr.aria-label]="altText() ?? null"
+      [attr.aria-hidden]="altText() ? null : true"
+    >
+      <ng-content />
+    </svg>
+  `,
+})
+export class SvgComponent {
+  altText = input<string>();
+  class = input<string>();
+  size = input<number>(24);
+}
