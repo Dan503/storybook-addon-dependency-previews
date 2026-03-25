4a93e23f7018e636aa17afe11324cf2bebd35241
Daniel Tonon <dan503.npm@gmail.com>
2026-03-25 12:00:01 +1100

    ChildContentAtom meta component

---
+++ example-site/angular/src/components/zz-meta-components/ChildContentAtom.component.ts
@@ -0,0 +1,16 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+	selector: 'child-content-atom',
+	template: `
+		<div
+			class="ChildContentAtom grid min-h-60 place-items-center rounded-md border-2 border-dashed bg-gray-200 p-4 text-black"
+		>
+			<p>Placeholder child content</p>
+		</div>
+	`,
+	standalone: true,
+})
+export class ChildContentAtomComponent {
+	class = input<string>('');
+}
