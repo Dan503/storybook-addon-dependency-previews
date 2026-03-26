712cb254547df289e1a37272ca0fe4aa7538c231
Daniel Tonon <dan503.npm@gmail.com>
2026-03-26 15:13:33 +1100

    Header Organism component

---
+++ example-site/angular/src/components/03-organisms/HeaderOrganism.stories.ts
@@ -0,0 +1,23 @@
+import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
+import type { StoryParameters } from 'storybook-addon-dependency-previews';
+import { HeaderOrganismComponent } from './HeaderOrganism.component';
+import { provideRouter } from '@angular/router';
+
+const meta: Meta<HeaderOrganismComponent> = {
+	title: '03 Organisms / Header Organism',
+	component: HeaderOrganismComponent,
+	tags: ['autodocs', 'organism'],
+	parameters: {
+		layout: 'padded',
+		__filePath: import.meta.url,
+	} satisfies StoryParameters,
+	decorators: [applicationConfig({ providers: [provideRouter([])] })],
+};
+
+export default meta;
+
+type Story = StoryObj<HeaderOrganismComponent>;
+
+export const Primary: Story = {
+	args: {},
+};
