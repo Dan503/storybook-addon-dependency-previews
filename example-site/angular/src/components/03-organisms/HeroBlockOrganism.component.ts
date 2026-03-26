f01005773d99b30428234fef960f13323e91a1e3
Daniel Tonon <dan503.npm@gmail.com>
2026-03-26 16:12:25 +1100

    HeroBlockOrganism component

---
+++ example-site/angular/src/components/03-organisms/HeroBlockOrganism.component.ts
@@ -0,0 +1,36 @@
+import { Component, input, TemplateRef } from '@angular/core';
+import { BgImageContainerAtomComponent } from '../01-atoms/BgImageContainerAtom.component';
+import { ScreenPaddingAtomComponent } from '../01-atoms/ScreenPaddingAtom.component';
+import { StringOrTemplateAtomComponent } from '../01-atoms/StringOrTemplateAtom.component';
+
+@Component({
+	selector: 'hero-block-organism',
+	template: `
+		<bg-image-container-atom
+			[imgSrc]="imgSrc()"
+			[tintPercent]="tintPercentage()"
+			[tintColor]="tintColor()"
+		>
+			<screen-padding-atom [padVertical]="true">
+				<h1 class="text-4xl font-bold text-center">
+					<string-or-template-atom [value]="title()" />
+				</h1>
+				<ng-content />
+			</screen-padding-atom>
+		</bg-image-container-atom>
+	`,
+	standalone: true,
+	imports: [
+		BgImageContainerAtomComponent,
+		ScreenPaddingAtomComponent,
+		StringOrTemplateAtomComponent,
+	],
+})
+export class HeroBlockOrganismComponent {
+	class = input<string>('');
+	title = input<string | TemplateRef<unknown>>('');
+	imgSrc = input<string>('');
+	tintPercentage = input<number>(70);
+	tintColor = input<string>('white');
+	altText = input<string>('');
+}
