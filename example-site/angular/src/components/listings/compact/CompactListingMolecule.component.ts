7c1c8fa99d0f7e70a29b78e5eadcd85cfdfbc558
Daniel Tonon <dan503.npm@gmail.com>
2026-03-30 15:43:04 +1100

    CompactListingMolecule component

---
+++ example-site/angular/src/components/listings/compact/CompactListingMolecule.component.ts
@@ -0,0 +1,31 @@
+import { Component, input } from '@angular/core';
+import type { AngularComponentProps } from 'storybook-addon-dependency-previews';
+
+@Component({
+	selector: 'compact-listing-molecule',
+	template: `
+		<div
+			[class]="'CompactListingMolecule grid grid-cols-[auto_1fr] items-center gap-4 ' + class()"
+		>
+			<img [src]="imageSrc()" alt="" class="h-15" />
+			<div>
+				<h3 class="text-xl leading-none font-bold">{{ title() }}</h3>
+				<p>{{ description() }}</p>
+			</div>
+		</div>
+	`,
+	standalone: true,
+	imports: [],
+})
+export class CompactListingMoleculeComponent {
+	class = input<string>('');
+	imageSrc = input<string>('');
+	title = input<string>('');
+	description = input<string>('');
+	href = input<string>('');
+}
+
+export type CompactListingMolecule = AngularComponentProps<
+	CompactListingMoleculeComponent,
+	'class'
+>;
