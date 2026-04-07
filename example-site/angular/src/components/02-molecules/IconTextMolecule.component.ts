b3e52ea4a4f556c8614ab13fa82bf9b134718521
Daniel Tonon <dan503.npm@gmail.com>
2026-04-07 13:23:41 +1000

    Fix for error "Index signature for type 'string' is missing in type 'IconProps'"

--- example-site/angular/src/components/02-molecules/IconTextMolecule.component.ts
+++ example-site/angular/src/components/02-molecules/IconTextMolecule.component.ts
@@ -1,12 +1,12 @@
-import { Component, input } from '@angular/core'
-import { NgComponentOutlet } from '@angular/common'
-import type { IconComponent, IconProps } from '../01-atoms/icons/iconTypes'
+import { Component, input } from '@angular/core';
+import { NgComponentOutlet } from '@angular/common';
+import type { IconComponent, IconProps } from '../01-atoms/icons/iconTypes';
 
 @Component({
 	selector: 'icon-text-molecule',
 	template: `
 		<p class="IconTextMolecule flex items-center gap-1 text-lg font-medium text-gray-900">
-			<ng-container *ngComponentOutlet="icon(); inputs: iconInputs" />
+			<ng-container *ngComponentOutlet="icon()" />
 			{{ text() }}
 		</p>
 	`,
@@ -14,7 +14,7 @@ import type { IconComponent, IconProps } from '../01-atoms/icons/iconTypes'
 	imports: [NgComponentOutlet],
 })
 export class IconTextMoleculeComponent {
-	icon = input.required<IconComponent>()
-	text = input<string>('')
-	protected readonly iconInputs: IconProps = { class: 'h-[1em] w-[1em]' }
+	icon = input.required<IconComponent>();
+	text = input<string>('');
+	protected readonly iconInputs: IconProps = { class: 'h-[1em] w-[1em]' };
 }
