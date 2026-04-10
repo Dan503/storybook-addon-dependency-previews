499995c0427ccbc64e1e2e5a23d2a83882be67d5
Daniel Tonon <dan503.npm@gmail.com>
2026-04-10 10:07:29 +1000

    hover styling on nav link

--- example-site/angular/src/components/02-molecules/MainNavMolecule.component.ts
+++ example-site/angular/src/components/02-molecules/MainNavMolecule.component.ts
@@ -5,11 +5,19 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 	selector: 'main-nav-molecule',
 	template: `
 		<nav class="flex gap-4 font-bold">
-			<a routerLink="/" routerLinkActive="underline" [routerLinkActiveOptions]="{ exact: true }"
+			<a
+				routerLink="/"
+				class="hover:underline"
+				routerLinkActive="underline"
+				[routerLinkActiveOptions]="{ exact: true }"
 				>Home</a
 			>
-			<a routerLink="/categories" routerLinkActive="underline">Food categories</a>
-			<a routerLink="/contact-us" routerLinkActive="underline">Contact us</a>
+			<a routerLink="/categories" class="hover:underline" routerLinkActive="underline"
+				>Food categories</a
+			>
+			<a routerLink="/contact-us" class="hover:underline" routerLinkActive="underline"
+				>Contact us</a
+			>
 		</nav>
 	`,
 	standalone: true,
