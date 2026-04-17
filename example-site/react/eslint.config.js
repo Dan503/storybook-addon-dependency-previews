6f0778574b9bb227150ae972491d590110edefac
Daniel Tonon <dan503.npm@gmail.com>
2026-04-17 13:22:48 +1000

    Fixing ES Lint and TS errors caused by broken config files

--- example-site/react/eslint.config.js
+++ example-site/react/eslint.config.js
@@ -1,8 +1,35 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook'
 
 //  @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
 
-export default [...tanstackConfig, ...storybook.configs["flat/recommended"]];
+const tanstackConfigWithRoot = tanstackConfig.map((config) => {
+	if (config.languageOptions?.parserOptions?.project === true) {
+		return {
+			...config,
+			languageOptions: {
+				...config.languageOptions,
+				parserOptions: {
+					...config.languageOptions.parserOptions,
+					tsconfigRootDir: import.meta.dirname,
+				},
+			},
+		}
+	}
+	return config
+})
+
+export default [
+	...tanstackConfigWithRoot,
+	...storybook.configs['flat/recommended'],
+	{
+		rules: {
+			'@typescript-eslint/naming-convention': [
+				'error',
+				{ selector: 'typeParameter', format: ['StrictPascalCase'] },
+			],
+		},
+	},
+]
