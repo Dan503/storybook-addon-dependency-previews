fd1c82f6cdba308a48660d9a7179e8bbe3b2e25f
Daniel Tonon <dan503.npm@gmail.com>
2026-05-15 11:13:50 +1000

    Patching React Formisch to export `useSignals()`
    `useSignals` is essential for `FormDataPreviewAtom` to update on every key stroke

--- example-site/react/src/components/forms/FormDataPreview/FormDataPreviewAtom.tsx
+++ example-site/react/src/components/forms/FormDataPreview/FormDataPreviewAtom.tsx
@@ -1,10 +1,11 @@
-import { getInput } from '@formisch/react'
+import { getInput, useSignals } from '@formisch/react'
 import type { Schema } from '@formisch/react'
 import type { WithForm } from '../FormTypes'
 
 export function FormDataPreviewAtom<TSchema extends Schema>({
 	form,
 }: WithForm<TSchema>) {
+	useSignals()
 	const formValues = getInput(form)
 
 	return (
