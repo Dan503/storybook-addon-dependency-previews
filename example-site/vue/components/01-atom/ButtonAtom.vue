bf2394c6aee8ef443f93f344d360c994e2f1de3b
Daniel Tonon <dan503.npm@gmail.com>
2026-05-26 14:02:22 +1000

    Button Atom vue component

---
+++ example-site/vue/components/01-atom/ButtonAtom.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+export interface PropsForButtonAtom {
+	type?: 'button' | 'submit' | 'reset'
+	onClick?: (event: MouseEvent) => void
+}
+
+const { type = 'button', onClick } = defineProps<PropsForButtonAtom>()
+</script>
+
+<template>
+	<button
+		:type="type"
+		class="bg-teal-200 hover:bg-teal-100 focus:bg-teal-100 border-2 border-teal-900 px-4 py-1 rounded-lg"
+		@click="onClick"
+	>
+		<slot />
+	</button>
+</template>
