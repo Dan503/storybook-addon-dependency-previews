eb2f1a663f4c1feefb71c8fa10ad16cf480a87c7
Daniel Tonon <dan503.npm@gmail.com>
2026-05-26 18:43:18 +1000

    ScreenPaddingAtom.vue

---
+++ example-site/vue/components/01-atoms/ScreenPaddingAtom.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+export interface PropsForScreenPaddingAtom {
+	padVertical?: boolean
+}
+
+const { padVertical } = defineProps<PropsForScreenPaddingAtom>()
+</script>
+
+<template>
+	<div class="ScreenPaddingAtom w-full" :class="padVertical ? 'p-6' : 'px-6'">
+		<slot />
+	</div>
+</template>
