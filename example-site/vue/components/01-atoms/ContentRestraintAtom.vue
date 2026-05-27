97c6543a3c6bc116594812585b82d6ab6b0ab5e3
Daniel Tonon <dan503.npm@gmail.com>
2026-05-27 12:39:25 +1000

    ContentRestraintAtom.vue

---
+++ example-site/vue/components/01-atoms/ContentRestraintAtom.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import ScreenPaddingAtom from './ScreenPaddingAtom.vue'
+
+export interface PropsForContentRestraintAtom {
+	padVertical?: boolean
+}
+
+const { padVertical } = defineProps<PropsForContentRestraintAtom>()
+</script>
+
+<template>
+	<ScreenPaddingAtom :pad-vertical="padVertical">
+		<div
+			class="ContentRestraintAtom grid grid-cols-[1fr_minmax(auto,800px)_1fr]"
+		>
+			<div class="width-full col-start-2 h-full">
+				<slot />
+			</div>
+		</div>
+	</ScreenPaddingAtom>
+</template>
