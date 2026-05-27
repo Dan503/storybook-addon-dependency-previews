b0f4d158728c370e31282c03a45fae2ed9f54a12
Daniel Tonon <dan503.npm@gmail.com>
2026-05-27 15:25:16 +1000

    IconTextMolecule.vue

---
+++ example-site/vue/components/02-molecules/IconTextMolecule.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { IconComponent } from '../01-atoms/icons/iconTypes'
+
+export interface PropsForIconTextMolecule {
+	Icon: IconComponent
+}
+
+const { Icon } = defineProps<PropsForIconTextMolecule>()
+</script>
+
+<template>
+	<p
+		class="IconTextMolecule flex items-center gap-1 text-lg font-medium text-gray-900"
+	>
+		<Icon class="h-[1em] w-[1em]" />
+		<slot />
+	</p>
+</template>
