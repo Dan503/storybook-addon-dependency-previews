0ce4a342daf31ad0188aeb43ba1e084593ab2d4c
Daniel Tonon <dan503.npm@gmail.com>
2026-05-28 14:52:59 +1000

    HeaderOrganism.vue

---
+++ example-site/vue/components/03-organisms/HeaderOrganism.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import ScreenPaddingAtom from '../01-atoms/ScreenPaddingAtom.vue'
+import MainNavMolecule from '../02-molecules/MainNavMolecule.vue'
+import logo from '../../app/assets/simplified-logo.png'
+
+export interface PropsForHeaderOrganism {}
+
+const {} = defineProps<PropsForHeaderOrganism>()
+</script>
+
+<template>
+	<header
+		class="HeaderOrganism border-b-2 border-teal-900 bg-teal-100 p-2 text-black"
+	>
+		<ScreenPaddingAtom>
+			<div
+				class="flex w-full items-center justify-between gap-2 max-md:flex-col max-md:justify-center"
+			>
+				<NuxtLink to="/" class="flex items-center gap-2">
+					<img :src="logo" alt="Logo" :height="50" :width="50" />
+					<p class="text-3xl font-extrabold">The Meal Place</p>
+				</NuxtLink>
+				<MainNavMolecule />
+			</div>
+		</ScreenPaddingAtom>
+	</header>
+</template>
