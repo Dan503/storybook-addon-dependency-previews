af67f61ff6c41683fc563740bbc79b83b10635f9
Daniel Tonon <dan503.npm@gmail.com>
2026-08-04 10:22:18 +1000

    HeaderOrganism

---
+++ example-site/solid/src/components/03-organisms/HeaderOrganism.tsx
@@ -0,0 +1,23 @@
+import { ScreenPaddingAtom } from '../01-atoms/ScreenPaddingAtom'
+import { MainNavMolecule } from '../02-molecules/MainNavMolecule'
+
+export function HeaderOrganism() {
+	return (
+		<header class="p-2 bg-teal-100 border-b-2 border-teal-900 text-black">
+			<ScreenPaddingAtom>
+				<div class="flex w-full items-center justify-between gap-2 max-md:flex-col max-md:justify-center">
+					<a href="/" class="flex items-center gap-2">
+						<img
+							src="/simplified-logo.png"
+							alt="Logo"
+							height={50}
+							width={50}
+						/>
+						<p class="font-extrabold text-3xl">The Meal Place</p>
+					</a>
+					<MainNavMolecule />
+				</div>
+			</ScreenPaddingAtom>
+		</header>
+	)
+}
