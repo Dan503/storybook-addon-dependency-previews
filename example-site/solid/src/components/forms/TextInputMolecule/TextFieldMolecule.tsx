795b7a4e83d24e55ab3410355d1f8e2edc2d59dd
Daniel Tonon <dan503.npm@gmail.com>
2026-08-04 11:30:45 +1000

    TextFieldMolecule component

---
+++ example-site/solid/src/components/forms/TextInputMolecule/TextFieldMolecule.tsx
@@ -0,0 +1,40 @@
+import { ErrorListMolecule } from '../ErrorMessages/ErrorListMolecule'
+import type { RequiredPath, FormSchema } from '@formisch/solid'
+import type { WithField } from '../FormTypes'
+
+export interface PropsForTextFieldMolecule {
+	label: string
+	placeholder?: string
+}
+
+export type FieldPropsForTextFieldMolecule<
+	TSchema extends FormSchema,
+	TPath extends RequiredPath,
+> = PropsForTextFieldMolecule & WithField<TSchema, TPath>
+
+export function TextFieldMolecule<
+	TSchema extends FormSchema,
+	TPath extends RequiredPath,
+>({
+	label,
+	placeholder,
+	field,
+}: FieldPropsForTextFieldMolecule<TSchema, TPath>) {
+	const id = `ID-${label.replace(/\W/g, '')}`
+	const showErrors = (field.errors?.length ?? 0) > 0
+
+	return (
+		<div>
+			<label for={id} class="block mb-1">
+				<span class="text-xl font-bold">{label}</span>
+				<input
+					{...field.props}
+					id={id}
+					placeholder={placeholder}
+					class={`w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 ${showErrors ? 'placeholder-red-900/60' : ''}`}
+				/>
+			</label>
+			{showErrors && <ErrorListMolecule errors={field.errors ?? []} />}
+		</div>
+	)
+}
