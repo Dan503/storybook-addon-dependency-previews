feeb041d252edb89a38a5dd2f3d8aa8377277a72
Daniel Tonon <dan503.npm@gmail.com>
2026-08-04 13:29:59 +1000

    ExternalLinkAtom

---
+++ example-site/solid/src/components/01-atoms/ExternalLinkAtom.tsx
@@ -0,0 +1,22 @@
+import type { JSX } from 'solid-js'
+import { ExternalLinkIcon } from './icons/ExternalLinkIcon'
+
+export interface PropsForExternalLinkAtom {
+	href: string
+	children?: JSX.Element
+}
+
+export function ExternalLinkAtom({ href, children }: PropsForExternalLinkAtom) {
+	return (
+		<a
+			class="ExternalLinkAtom inline-flex items-center gap-1 text-teal-700 hover:underline hover:text-teal-900"
+			target="_blank"
+			rel="noopener noreferrer"
+			href={href}
+			title="Opens in new tab"
+		>
+			{children}
+			<ExternalLinkIcon />
+		</a>
+	)
+}
