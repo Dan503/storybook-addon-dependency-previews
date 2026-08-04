1bb18775b7e9eda69d9b95bd6a15d81b34c8a27c
Daniel Tonon <dan503.npm@gmail.com>
2026-08-04 14:22:35 +1000

    Home page route

--- example-site/solid/src/routes/index.tsx
+++ example-site/solid/src/routes/index.tsx
@@ -1,19 +1,14 @@
-import { Title } from "@solidjs/meta";
-import Counter from "~/components/Counter";
+import { Title } from '@solidjs/meta'
+import { HomeTemplate } from '../components/04-templates/HomeTemplate'
+import { featuredMealsData } from 'example-site-shared/data'
 
 export default function Home() {
-  return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
-  );
+	return (
+		<>
+			<Title>
+				The Meal Place - The Storybook Dependency Previews Example Site
+			</Title>
+			<HomeTemplate featuredMeals={featuredMealsData} />
+		</>
+	)
 }
